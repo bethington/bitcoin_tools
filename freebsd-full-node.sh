fdbbcfc1e2f8dd71fdfce3ace295dd9d09a20b60
Benjamin Ethington <bethington@iccu.com>
2019-01-28 00:33:28 -0700

    Update freebsd-full-node.sh

--- freebsd-full-node.sh
+++ freebsd-full-node.sh
@@ -1,7 +1,8 @@
 mkdir /var/db/bitcoin/
-edit /usr/local/etc/bitcoin.conf
 cd /usr/local/etc/rc.d
 fetch -o /usr/local/etc/rc.d/bitcoin https://raw.githubusercontent.com/bethington/bitcoin_tools/master/bitcoin.rc.d
+fetch -o /usr/local/etc/bitcoin.conf https://raw.githubusercontent.com/bethington/bitcoin_tools/master/bitcoin.conf
+edit /usr/local/etc/bitcoin.conf
 chmod 555 /usr/local/etc/rc.d/bitcoin
 echo "bitcoin_enable="YES"" >> /etc/rc.conf
 service bitcoin start
