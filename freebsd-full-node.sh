3adee5cce169c172ee1e3499acc27b99b4fa5a60
Benjamin Ethington <bethington@iccu.com>
2019-01-27 22:42:32 -0700

    Update freebsd-full-node.sh

--- freebsd-full-node.sh
+++ freebsd-full-node.sh
@@ -1,1 +1,10 @@
-
+pkg install autoconf automake boost-libs git gmake libevent libtool openssl pkgconf python3
+cd /home
+mkdir root
+cd root
+git clone https://github.com/bitcoin/bitcoin
+cd bitcoin
+./autogen.sh
+./configure --without-gui --disable-wallet
+gmake
+gmake install
